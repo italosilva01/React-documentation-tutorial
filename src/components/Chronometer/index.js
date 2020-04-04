eb147e0ffcc982f39c6aa211ba65e623ee2d377a
italosilva01 <italo.silvan87@gmail.com>
2020-04-04 17:41:05 -0300

    formatting the seconds

--- src/components/Chronometer/index.js
+++ src/components/Chronometer/index.js
@@ -13,22 +13,19 @@ class Chronometer extends React.Component{
   }
   
     render(){
-      console.log(this.props.timeFinal);
-      console.log(this.state.timeInitial);
-      console.log(Math.round(this.props.timeFinal - this.state.timeInitial));
+      
       if(this.props.timeFinal){
         return(
           <div className="clock">
-                <h1>{Math.round(this.props.timeFinal - this.state.timeInitial)}</h1>
-            </div> 
+            <h1>{Math.round(this.props.timeFinal - this.state.timeInitial)/1000} Segundos</h1>
+          </div> 
         )
       }
-     return(
-            <div className="clock">
-                <h1></h1>
-                <p></p>
-            </div>  
-        )
+      return (
+        <>
+          
+        </>
+      );
     }
 }
 
