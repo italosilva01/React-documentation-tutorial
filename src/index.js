0c35cbc536b137e6614262406bcf7aa09d96ca84
italosilva01 <italo.silvan87@gmail.com>
2020-04-06 19:04:21 -0300

    organized project

--- src/index.js
+++ src/index.js
@@ -1,55 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Clock from './components/Clock/index'
-import Chronometer from './components/Chronometer/index'
+import Clock from './components/Clock'
+import Chronometer from './components/Chronometer'
+import Board from './components/Board'
 
-
-function Square(props){
-  //This function renders square
-  return(
-    //props.onClick and props.value  
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
-
-  
-  class Board extends React.Component {
- 
-    renderSquare(i) {
-      return (<Square 
-      value={this.props.squares[i]}
-      onClick={()=>this.props.onClick(i)}
-       />);
-    }
-  
-    render() {
-      return (
-
-        <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
   
   class Game extends React.Component {
     constructor(props){
@@ -139,7 +94,7 @@ function Square(props){
       //HISTORY
       const moves = history.map((step,move)=>{
         const desc = move?
-        'Go to move #'+move+' ':
+        'Go to move #'+move:
         'Go to game start';
 
         return(
