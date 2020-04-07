ce274d07b1e5cbf3a4f0437aca1ba5adcadd6494
italosilva01 <italo.silvan87@gmail.com>
2020-04-07 16:37:19 -0300

    update style

--- src/index.js
+++ src/index.js
@@ -111,17 +111,17 @@ import Board from './components/Board'
       }
       return (
         <div className="game">
+          <div className="game-info">
+            <div>{status}</div>
+            <ol></ol>
+            
+          </div>
+
           <div className="game-board">
             <Board 
               squares = {current.squares}
               onClick={(i)=>this.handleClick(i)}
             />
-          </div>
-          <div className="game-info">
-            <div>{status}</div>
-            <ol></ol>
-            <Clock/>
-            <Chronometer timeFinal={timeFinal}/>
           </div>
         </div>
       );
